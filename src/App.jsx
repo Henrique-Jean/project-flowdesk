4c046c8dc8f98102aa4843d694cf160a2ca00753
Henrique_Jean <j.darkline.dev@pm.me>
2026-02-03 11:15:36 -0300

    feat/feat: initial project structure with vite + react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign:'center', padding: '50px'}}>
+      <h1>🚀 FlowDesk</h1>
+      <p>The beginning of our SaaS.</p>
+    </div>
+  )
+}
+
+export default App
