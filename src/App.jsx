4f61a82dfb05e198895bacb73fd83efcab8bee07
Henrique_Jean <j.darkline.dev@pm.me>
2026-02-04 16:01:18 -0300

    fix: add missing return in App and fix styles in Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Header from "./components/header";
 
 function App() {
-  <div className="min-h-screen bg-slate-50">
-    <Header />
+  return(
+      <div className="min-h-screen bg-slate-50">
+      <Header />
 
-    <main className="container mx-auto px-6 py-20 text-center">
-      <h1 className="text-4xl font-bold text-gray-900">
+      <main className="container mx-auto px-6 py-20 text-center">
+        <h1 className="text-4xl font-bold text-gray-900">
         O Header está Pronto! ✅
-      </h1>
+        </h1>
 
-      <p className="mt-4 text-gray-600">
+        <p className="mt-4 text-gray-600">
         Agora vamos contruir a próxima peça: o Hero Section.
-      </p>
-    </main>
-  </div>
+       </p>
+      </main>
+    </div>
+  )
 }
 export default App
